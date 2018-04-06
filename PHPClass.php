cf9a232f658cf9311b58cd937572c1cd8d69b2ae
Om <Om>
2018-04-06 08:57:28 +0530

    This is my second commit.

---
+++ PHPClass.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php  
+for ($x = 0; $x <= 10; $x++) {
+  echo "The number is: $x <br>";
+}
+?>
+<html>
+<body>
+
+<?php  
+/*$colors = array("red", "green", "blue", "yellow"); 
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
+*/?>  
+
+</body>
+</html>
